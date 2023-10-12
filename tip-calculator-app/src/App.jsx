93a00eb40bd07b09ecfb0d7f870345e7bc90a238
Timothy Kwok <timothykwok95@gmail.com>
2023-10-12 17:46:27 +0800

    main-body-text done

---
+++ tip-calculator-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../src/App.css';
+
+const App = () => {
+	return (
+		<>
+			<div id='main-body'>
+				<div id='main-body-text'>
+					<p>SPLITTER</p>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default App;
